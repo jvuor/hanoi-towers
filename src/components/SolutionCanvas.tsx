a038609ccda30dc980b62f68bc8067349858c291
jvuor <jussi.vuorinen@gmail.com>
2019-05-11 09:07:43 +0300

    use the palette fully, draw disc outlines

--- src/components/SolutionCanvas.tsx
+++ src/components/SolutionCanvas.tsx
@@ -58,16 +58,21 @@ export default class SolutionCanvas extends React.Component<SolutionCanvasProps>
     this.drawBase();
   }
 
-  getHeigth = () => (this.props.discs * 20) + 60;
   getWidthOfTheLargestDisc = () => 40 + (20 * this.props.discs)
+  // height and width of the canvas
+  getHeigth = () => (this.props.discs * 20) + 60;
   getWidth = () => this.getWidthOfTheLargestDisc() * this.rodsNr + (20 * this.rodsNr);
 
+  /**
+   * Draws the background and the rods
+   */
   drawBase() {
     const widthOfTheLargestDisc = this.getWidthOfTheLargestDisc();
     const width = this.getWidth();
     const height = this.getHeigth();
     const rodHeight = height - 40;
 
+    // calculating the places for the rods
     let rodSpot = 20 + widthOfTheLargestDisc / 2;
     let rodInterval = widthOfTheLargestDisc + 10;
     this.rods = new Array(this.rodsNr).fill(null);
@@ -77,9 +82,11 @@ export default class SolutionCanvas extends React.Component<SolutionCanvasProps>
       return spot
     });
 
+    // fill background
     this.ctx.fillStyle = '#ffffff';
     this.ctx.fillRect(0, 0, width, height);
 
+    // draw the ground and the rods
     this.ctx.beginPath();
     this.ctx.lineWidth = 4;
     this.ctx.moveTo(0, height - 10);
@@ -94,15 +101,33 @@ export default class SolutionCanvas extends React.Component<SolutionCanvasProps>
     this.drawDiscs();
   }
 
+  /**
+   * Draws the discs
+   */
   drawDiscs() {
+    // this keeps count of how many discs there already are on a rod so we can calculate
+    // the correct y position
     const discsAtRod = Array(this.rodsNr).fill(0);
-    let discWidth = 40 + (20 * this.props.discs)
+    let discWidth = this.getWidthOfTheLargestDisc();
+
+    // drawing the discs one at a time, starting from the largest
     this.props.discLocations.forEach((disc, i) => {
+
+      // calculating where to draw the disc
       const discInitY = this.getHeigth() - 10 - (discsAtRod[disc] * 20) - 20;
       const discInitX = this.rods[disc] - (discWidth / 2);
 
-      this.ctx.fillStyle = palette[i];
+      // pick a color from the palette
+      this.ctx.fillStyle = palette[Math.floor((i / this.props.discs)* 20)];
+
+      // draw the disc
       this.ctx.fillRect(discInitX, discInitY, discWidth, 20);
+
+      // draw the outline for the disc
+      this.ctx.strokeStyle = 'black';
+      this.ctx.lineWidth = 3;
+      this.ctx.strokeRect(discInitX, discInitY, discWidth, 20)
+
       discWidth -= 20;
       discsAtRod[disc] += 1;
     })
